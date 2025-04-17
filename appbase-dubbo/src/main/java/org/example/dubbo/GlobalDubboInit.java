1829a232cd9064badd75e3dd81626e2bf103dd4d
yangming <1066537334@qq.com>
2025-04-17 15:06:30 +0800

    sentinel全局配置

--- appbase-dubbo/src/main/java/org/example/dubbo/GlobalDubboInit.java
+++ appbase-dubbo/src/main/java/org/example/dubbo/GlobalDubboInit.java
@@ -2,6 +2,8 @@ package org.example.dubbo;
 
 import com.alibaba.csp.sentinel.adapter.dubbo3.config.DubboAdapterGlobalConfig;
 import org.apache.dubbo.rpc.AsyncRpcResult;
+import org.example.common.BusinessException;
+import org.example.common.constant.RespInfo;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -11,13 +13,16 @@ public class GlobalDubboInit {
     @PostConstruct
     public void init() {
         DubboAdapterGlobalConfig.setProviderFallback((invoker, invocation, ex) -> {
-            System.out.println("Blocked by Sentinel: " + ex.getClass().getSimpleName() + ", " + invocation);
+            //System.out.println("Blocked by Sentinel: " + ex.getClass().getSimpleName() + ", " + invocation);
+            String message = "Dubbo Provider Blocked by Sentinel: " + ex.getClass().getSimpleName() + ", " + invocation;
+            BusinessException businessException = new BusinessException(RespInfo.HTTP_ERROR.getCode(), message, true);
             return AsyncRpcResult.newDefaultAsyncResult(ex.toRuntimeException(), invocation);
         });
         DubboAdapterGlobalConfig.setConsumerFallback((invoker, invocation, ex) -> {
-            System.out.println("Blocked by Sentinel: " + ex.getClass().getSimpleName() + ", " + invocation);
-
-            return AsyncRpcResult.newDefaultAsyncResult(ex.toRuntimeException(), invocation);
+            //System.out.println("Blocked by Sentinel: " + ex.getClass().getSimpleName() + ", " + invocation);
+            String message = "Dubbo Consumer Blocked by Sentinel: " + ex.getClass().getSimpleName() + ", " + invocation;
+            BusinessException businessException = new BusinessException(RespInfo.HTTP_ERROR.getCode(), message, true);
+            return AsyncRpcResult.newDefaultAsyncResult(businessException, invocation);
         });
     }
 }
